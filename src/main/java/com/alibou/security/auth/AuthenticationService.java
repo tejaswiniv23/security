f24896a31eb3a2d0ac0a2fe1d71a32ab8431473a
tejaswiniv23 <tejaswini.vadje@neosoftmail.com>
2023-04-23 21:09:56 +0530

    Initial commit

---
+++ src/main/java/com/alibou/security/auth/AuthenticationService.java
@@ -0,0 +1,96 @@
+package com.alibou.security.auth;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.alibou.security.config.JwtService;
+import com.alibou.security.token.Token;
+import com.alibou.security.token.TokenRepository;
+import com.alibou.security.token.TokenType;
+import com.alibou.security.user.Role;
+import com.alibou.security.user.User;
+import com.alibou.security.user.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+	private final UserRepository repository;
+	private final TokenRepository tokenRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
+	private final AuthenticationManager authenticationManager;
+
+	public AuthenticationResponse register(RegisterRequest request) {
+		User user = User.builder().firstname(request.getFirstname()).lastname(request.getLastname())
+				.email(request.getEmail()).password(passwordEncoder.encode(request.getPassword())).role(Role.USER)
+				.build();
+		User savedUser = repository.save(user);
+		String jwtToken = jwtService.generateToken(user);
+		String refreshToken = jwtService.generateRefreshToken(user);
+		saveUserToken(savedUser, jwtToken);
+		return AuthenticationResponse.builder().accessToken(jwtToken).refreshToken(refreshToken).build();
+	}
+
+	public AuthenticationResponse authenticate(AuthenticationRequest request) {
+		authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+		User user = repository.findByEmail(request.getEmail()).orElseThrow(null);
+		String jwtToken = jwtService.generateToken(user);
+		String refreshToken = jwtService.generateRefreshToken(user);
+		revokeAllUserTokens(user);
+		saveUserToken(user, jwtToken);
+		return AuthenticationResponse.builder().accessToken(jwtToken).refreshToken(refreshToken).build();
+	}
+
+	private void saveUserToken(User user, String jwtToken) {
+		Token token = Token.builder().user(user).token(jwtToken).tokenType(TokenType.BEARER).expired(false)
+				.revoked(false).build();
+		tokenRepository.save(token);
+	}
+
+	private void revokeAllUserTokens(User user) {
+		List<Token> validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+		if (validUserTokens.isEmpty())
+			return;
+		validUserTokens.forEach(token -> {
+			token.setExpired(true);
+			token.setRevoked(true);
+		});
+		tokenRepository.saveAll(validUserTokens);
+	}
+
+	public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+		final String refreshToken;
+		final String userEmail;
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			return;
+		}
+		refreshToken = authHeader.substring(7);
+		userEmail = jwtService.extractUsername(refreshToken);
+		if (userEmail != null) {
+			User user = this.repository.findByEmail(userEmail).orElseThrow(null);
+			if (jwtService.isTokenValid(refreshToken, user)) {
+				String accessToken = jwtService.generateToken(user);
+				revokeAllUserTokens(user);
+				saveUserToken(user, accessToken);
+				AuthenticationResponse authResponse = AuthenticationResponse.builder()
+						.accessToken(accessToken)
+						.refreshToken(refreshToken)
+						.build();
+				new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+			}
+		}
+	}
+}
